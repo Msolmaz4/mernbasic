5c652364c6005208455524b8fbcf00e3d43e07a7
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-11-25 23:19:35 +0100

    routerProductController

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,13 +1,10 @@
 const express = require('express')
 
 const router = express.Router()
+const { getAllProducts,getProductById } =require('../controller/productControllers')
 
 //hepsini cagirmak icin
-router.get('/',(req,res)=>{
-
-})
+router.get('/',getAllProducts)
 //bir tane cagirmak icin id kullaniriy
-router.get('/:id',(req,res)=>{
-
-})
+router.get('/:id',getProductById)
 module.exports = router
