5c540669844ed63ab404c409d13ff0f73a72e346
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-11-25 23:06:27 +0100

    productROUTES

---
+++ backend/routes/productsRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router()
+
+//hepsini cagirmak icin
+router.get('/',(req,res)=>{
+
+})
+//bir tane cagirmak icin id kullaniriy
+router.get('/:id',(req,res)=>{
+
+})
+module.exports = router
