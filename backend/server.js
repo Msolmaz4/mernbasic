69eb683b4371c66666af8bd6686766359f8d7b3a
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-11-29 12:05:56 +0100

    redux-clint-backend-verbinden

--- backend/server.js
+++ backend/server.js
@@ -16,3 +16,5 @@ app.use('/api/products',productsRoutes)
 
 
 app.listen(PORT,()=>console.log('okey'))
+//baglanti kurmak icuin packagejson client tarafinda proxy ekledik 
+//"proxy": "http://127.0.01:5000",
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -4,11 +4,23 @@ import './Home.css'
 import {useSelector,useDispatch} from 'react-redux'
 
 
+//actions
+import {getProducts as listProducts} from '../redux/actions/productActions'
+
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+  const getProducts =useSelector(state => state.getProducts)
+  const {products,loading,error} = getProducts
+//baglantiyi kiurunca proxy uzerinde consol reduxta urunleri gorduk
+  useEffect(()=>{
+    dispatch(listProducts())
+
+  },[dispatch])
 
 
-  
+
+
   return (
     <div className='homescreen'>
       <h2 className='homescreen_title'>Latest Products</h2>
